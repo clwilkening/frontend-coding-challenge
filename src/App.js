521eb456a478564e5ab23176e10cf3a6d12a53e3
Chris Wilkening <clw13clw13@gmail.com>
2017-03-15 11:30:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.getEvents = this.getEvents.bind(this);
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  getEvents(){
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Eventable</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
       </div>
     );
