90d1c2ff2844d160ca8deb7c577aa4b53092115e
Chris Wilkening <clw13clw13@gmail.com>
2017-03-15 15:33:11 -0400

    render dates

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 // import fetch from 'whatwg-fetch';
 
 class EventList extends Component {
@@ -16,15 +17,25 @@ class EventList extends Component {
     const events = this.props.events;
     let eventElements = [];
     events.forEach((event) => {
-      // console.log(event.title)
+        let start = moment(event.start_time).format('LLL');
+        let end = moment(event.end_time).format('LLL')
+
       eventElements.push(
         <div key={event.title}>
-          <h4>{event.title}</h4>
+          <a href={event.url} target="_blank"><h4>{event.title}</h4></a>
+
+          <p>Start: {start}</p>
+          <p>End: {end}</p>
         </div>
       )
     })
     return eventElements;
   }
+
+  // formatDates(){
+  //   const events = this.props.events;
+  //   let dates =
+  // }
 
   render() {
     return(
