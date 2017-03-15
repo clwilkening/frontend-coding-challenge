16b46821a379dec2f77b371a2c8587d7dc2ab382
Chris Wilkening <clw13clw13@gmail.com>
2017-03-15 11:45:38 -0400

    create EventList component

---
+++ src/components/EventList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import fetch from 'whatwg-fetch';
+
+class EventList extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return(
+
+    )
+  };
+};
+
+export default EventList;
